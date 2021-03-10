3058f4e8209bfd1b2dfa12a210708505fa348813
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-03-10 16:25:25 -0500

    carousel and logo icons with style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import ImageDisplay from './ImageDisplay';
 
 const Home = () => {
     return (
         <div>
             Welcome to Home.js
+            <ImageDisplay />
         </div>
     )
 }
