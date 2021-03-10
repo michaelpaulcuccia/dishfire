8cda8317224a2e734ac8c8dfa5ffeed45a46fb4f
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-03-10 10:08:46 -0500

    restarting project

--- config.js
+++
@@ -1,6 +0,0 @@
-const keys = {
-    "MONGO_URI": "mongodb+srv://dishfire001:dishadmin001@cluster0.vlprl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    "JWT_SECRET": "themoneyisunderthemattress"
-}
-
-export default keys;
