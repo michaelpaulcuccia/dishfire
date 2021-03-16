8f41083cc63fb99b370a4a0b546d9fd74bfe9386
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-03-16 10:45:53 -0400

    redux for login setup

---
+++ client/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { userLoginReducer } from './reducers/userReducers.js';
+
+const reducer = combineReducers({
+    userLogin: userLoginReducer
+});
+
+//get state from local storage - REFRESH
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage }
+};
+
+const middleware = [thunk];
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
